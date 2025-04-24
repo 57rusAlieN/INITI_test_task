580d21a70e18906c0c6c407e5c11b89127b0ab4e
Павел Кукшин <pkukshin@gmail.com>
2025-04-24 17:35:30 +0300

    kekeke

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 ################################################################################
 # Описание бинарного протокола
 ################################################################################
@@ -62,78 +62,192 @@ Serialisator::push должен быть именно шаблонной фун�
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <variant>
+#include <functional>
+#include <type_traits>
+
+class Any;
 
 using Id = uint64_t;
 using Buffer = std::vector<std::byte>;
 
-enum class TypeId : Id {
+enum class TypeId : Id
+{
     Uint,
     Float,
     String,
     Vector
 };
 
-class IntegerType {
+template<typename TDerived>
+class AnyBase
+{
+private:
+    /* data */
 public:
-    template<typename ...Args>
-    IntegerType(Args&& ...);
+
+template<typename TDerived>AnyBase
+(/* args */);
+
+template<typename TDerived>~AnyBase
+();
 };
 
-class FloatType {
+template<typename TDerived>
+AnyBase::AnyBase(/* args */)
+{
+}
+
+template<typename TDerived>
+AnyBase::~AnyBase()
+{
+}
+
+
+class IntegerType : public Any
+{
 public:
-    template<typename ...Args>
-    FloatType(Args&& ...);
+    IntegerType(uint64_t arg) : data(arg) 
+    {
+        serializeImplCallback = [this](Buffer& buf) { serializableImpl(buf); };
+        deserializeImplCallback = [this](Buffer::const_iterator begin, Buffer::const_iterator end) { deserializableImpl(begin,end); };
+    }
+protected:
+    uint64_t data;
+
+    void serializableImpl(Buffer& buf)
+    {
+        buf.clear();
+        buf.reserve(sizeof(_typeId) + sizeof(data));
+        const auto* tag_bytes = reinterpret_cast<const std::byte*>(&_typeId);
+        buf.insert(buf.end(), tag_bytes, tag_bytes + sizeof(_typeId)); 
+        
+        const auto* data_bytes = reinterpret_cast<const std::byte*>(&data);
+        buf.insert(buf.end(), data_bytes, data_bytes + sizeof(data));    
+    }
+    void deserializableImpl(Buffer::const_iterator begin, Buffer::const_iterator end)
+    {}
 };
 
-class StringType {
+class FloatType : public Any
+{
 public:
-    template<typename ...Args>
-    StringType(Args&& ...);
+    template <typename... Args>
+    FloatType(Args &&...);
 };
 
-class VectorType {
+class StringType : public Any
+{
 public:
-    template<typename ...Args>
-    VectorType(Args&& ...);
-
-    template<typename Arg>
-    void push_back(Arg&& _val);
+    template <typename... Args>
+    StringType(Args &&...);
 };
 
-class Any {
+class VectorType : public Any
+{
 public:
-    template<typename ...Args>
-    Any(Args&& ...);
+    template <typename... Args>
+    VectorType(Args &&...);
 
-    void serialize(Buffer& _buff) const;
-
-    Buffer::const_iterator deserialize(Buffer::const_iterator _begin, Buffer::const_iterator _end);
-
-    TypeId getPayloadTypeId() const;
-
-    template<typename Type>
-    auto& getValue() const;
-
-    template<TypeId kId>
-    auto& getValue() const;
-
-    bool operator == (const Any& _o) const;
+    template <typename Arg>
+    void push_back(Arg &&_val);
 };
 
-class Serializator {
+class Any
+{
 public:
-    template<typename Arg>
-    void push(Arg&& _val);
+    using PayloadType = std::variant<FloatType, IntegerType, StringType, VectorType>;
+    using DataType = std::variant<double, uint64_t, std::string, std::vector<Any>>;
+
+    template <typename... Args>//, typename = std::enable_if_t<std::is_constructible_v<PayloadType, Args...>>>
+    Any(Args &&...args) 
+        : data(std::forward(args...)) 
+        , serializeImpl(nullptr)
+        , deserializeImpl(nullptr)
+    {}
+
+    void serialize(Buffer &_buff) const 
+    {
+        if (serializeImplCallback != nullptr) serializeImplCallback(_buff);
+    }
+    Buffer::const_iterator deserialize(Buffer::const_iterator _begin, Buffer::const_iterator _end)
+    {
+        if (deserializeImplCallback != nullptr) deserializeImplCallback(_begin,_end);
+    }
+
+    TypeId getPayloadTypeId() const { return _typeId; }
+
+    template <typename Type, typename = std::variant<double, uint64_t, std::vector<Any>>>
+    auto &getValue() const { return std::get<Type>(data); }
+
+    template <TypeId kId>
+    auto &getValue() const
+    {
+        if constexpr (kId == TypeId::Float)
+        {
+            return getValue<double>(data);
+        }
+        else if constexpr (kId == TypeId::Uint)
+        {
+            return getValue<uint64_t>(data);
+        }
+        else if constexpr (kId == TypeId::String)
+        {
+            return getValue<std::string>(data);
+        }
+        else if constexpr (kId == TypeId::Vector)
+        {
+            return getValue<vector<Any>>(data);
+        }
+    }
+
+    bool operator==(const Any &_o) const
+    {
+        if (data.index() != _o.data.index())
+        {
+            return false;
+        }
+
+        return std::visit([&](const auto &val)
+                          {
+            using T = std::decay_t<decltype(val)>;
+            return val == std::get<T>(_o.data); }, data);
+    }
+
+protected:
+    TypeId _typeId;
+    DataType data;
+
+    void serializableImpl(Buffer& buf)
+    {
+        buf.clear();
+        buf.reserve(sizeof(_typeId) + sizeof(data));
+        const auto* tag_bytes = reinterpret_cast<const std::byte*>(&_typeId);
+        buf.insert(buf.end(), tag_bytes, tag_bytes + sizeof(_typeId)); 
+        
+        const auto* data_bytes = reinterpret_cast<const std::byte*>(&data);
+        buf.insert(buf.end(), data_bytes, data_bytes + sizeof(data));    
+    }
+
+    std::function<void(Buffer&)> serializeImplCallback;
+    std::function<void(Buffer::const_iterator, Buffer::const_iterator)> deserializeImplCallback;
+};
+
+class Serializator
+{
+public:
+    template <typename Arg, typename = std::enable_if_t<std::is_base_of_v<Any, Arg>>>
+    void push(Arg &&_val){}
 
     Buffer serialize() const;
 
-    static std::vector<Any> deserialize(const Buffer& _val);
+    static std::vector<Any> deserialize(const Buffer &_val);
 
-    const std::vector<Any>& getStorage() const;
+    const std::vector<Any> &getStorage() const;
 };
 
-
-int main() {
+int main()
+{
 
     std::ifstream raw;
     raw.open("raw.bin", std::ios_base::in | std::ios_base::binary);
@@ -144,12 +258,12 @@ int main() {
     raw.seekg(0, std::ios_base::beg);
 
     Buffer buff(size);
-    raw.read(reinterpret_cast<char*>(buff.data()), size);
+    raw.read(reinterpret_cast<char *>(buff.data()), size);
 
     auto res = Serializator::deserialize(buff);
 
     Serializator s;
-    for (auto&& i : res)
+    for (auto &&i : res)
         s.push(i);
 
     std::cout << (buff == s.serialize()) << '\n';
